e4da279795817be0f0a3419b958f558205dd5b3a
ssjlevon <140295647+ssjlevon@users.noreply.github.com>
2025-07-11 15:43:54 -0700

    Add files via upload

---
+++ TestEnvironment.ps1
@@ -0,0 +1,35 @@
+# Variables
+$location = "eastus"
+$vnetName = "vnet1"
+$addressPrefix = "10.0.0.0/16"
+$subnetName = "subnet1"
+$subnetPrefix = "10.0.0.0/24"
+
+$vm1Name = "VM-public"
+$vm1Username = "ssjuser"
+$vm1Password = "Password123!"  # Ensure you set a strong password
+
+# Create a resource group
+az group create --name MyResourceGroup --location $location
+
+# Create a virtual network with a subnet
+az network vnet create --name $vnetName --resource-group MyResourceGroup --location $location --address-prefix $addressPrefix --subnet-name $subnetName --subnet-prefix $subnetPrefix
+
+# Create a public IP address for VM1
+az network public-ip create --resource-group MyResourceGroup --name PublicIPVM1 --allocation-method Static
+
+# Create a network interface for VM1
+az network nic create --resource-group MyResourceGroup --name NICVM1 --vnet-name $vnetName --subnet $subnetName --public-ip-address PublicIPVM1
+
+# Create VM1
+az vm create --resource-group MyResourceGroup --name $vm1Name --location $location --nics NICVM1 --image Win2019Datacenter --admin-username $vm1Username --admin-password $vm1Password --size Standard_B1s
+
+
+# Output public IPs of the VMs
+Write-Output "VM1 Public IP:"
+az network public-ip show --resource-group MyResourceGroup --name PublicIPVM1 --query "ipAddress" --output tsv
+
+
+Set-AzVMExtension -ResourceGroupName "MyResourceGroup" -ExtensionName "IIS" -VMName "myVM" -Location "EastUS" -Publisher Microsoft.Compute -ExtensionType CustomScriptExtension -TypeHandlerVersion 1.8 -SettingString '{"commandToExecute":"powershell Add-WindowsFeature Web-Server; powershell Add-Content -Path \"C:\\inetpub\\wwwroot\\Default.htm\" -Value $($env:computername)"}'
+
+Get-AzPublicIPAddress -ResourceGroupName "MyResourceGroup" -Name "PublicIPVM1" | select IpAddress 
